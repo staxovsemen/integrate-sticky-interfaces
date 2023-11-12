bc9b85aa8c1460a07d8d7616cfa218dd18925311
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-11-12 02:41:46 -0500

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,26 @@
+def main():
+def generate_random_data():
+
+    main()
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+
+
+import random
+    return data
+        print(f"Random Number: {item}")
+    for item in data:        for (int item : data) {
+        }
+public class RandomDataGenerator {
+    }
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+
+    public static void main(String[] args) {
+        }
+
+}
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
